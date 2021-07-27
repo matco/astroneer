9404dd7917e8fb4055076840a45cb5abb9c3e3e5
Matthieu Corageoud <matthieu.corageoud@gmail.com>
2021-07-27 21:24:56 +0200

    Fix refresh of the home screen

--- src/home.ts
+++ src/home.ts
@@ -9,15 +9,15 @@ export const Home = {
 		Database.GetResources()
 			.sort(Things.Sort)
 			.map(r => Resources.DrawForList(r))
-			.forEach(Node.prototype.appendChild, document.getElementById('home_resources'));
+			.forEach(Node.prototype.appendChild, document.getElementById('home_resources').empty());
 		Database.GetItems()
 			.sort(Things.Sort)
 			.map(Items.DrawForList)
-			.forEach(Node.prototype.appendChild, document.getElementById('home_items'));
+			.forEach(Node.prototype.appendChild, document.getElementById('home_items').empty());
 		Database.GetPlanets()
 			.sort(Things.Sort)
 			.map(Planets.DrawForList)
-			.forEach(Node.prototype.appendChild, document.getElementById('home_planets'));
+			.forEach(Node.prototype.appendChild, document.getElementById('home_planets').empty());
 		document.getElementById('home').style.display = 'block';
 	}
 };
