73122cbe1bf64736c2690a4f9de2adaac489ac49
Markus Reisenhofer <markus.reisenhofer@gmail.com>
2022-10-24 17:46:26 +0200

    Refactored to make use of base interface

--- src/draw.ts
+++ src/draw.ts
@@ -1,20 +1,28 @@
-import {Item, Planet, Resource} from './types';
+import {Item, Planet, Resource, Thing} from './types';
 import {Router} from './router';
 import {Database} from './database';
 import {Labels} from './labels';
 import {ThingType} from './database';
 
 export const Utilities = {
-    DrawWiki: (item: Item): HTMLElement => {
-        const wiki = document.createFullElement('div', {class: "wiki", href: Router.GetWikiUrl(item)});
+    DrawWiki: (thing: Thing): HTMLElement => {
+        const wiki = document.createFullElement('div', {class: 'wiki'});
+        const link = document.createFullElement('a', {href: Router.GetWikiUrl(thing)});
+        if(thing.type === ThingType.Item || thing.type === ThingType.Resource) {
+            link.appendChild(document.createTextNode(`${Labels.GetLabel('wiki_link')} "${Labels.Localize(thing.label)}"`));
+        }
+        if(thing.type === ThingType.Planet) {
+            link.appendChild(document.createTextNode(`${Labels.GetLabel('wiki_link')} "${thing.name}"`));
+        }
+        wiki.appendChild(link)
         return wiki;
     },
-    DrawForList: (thing: Item | Planet): HTMLLIElement => {
+    DrawForList: (thing: Thing): HTMLLIElement => {
         const element = document.createFullElement('li');
         element.appendChild(Utilities.Draw(thing));
         return element;
     },
-    Draw: (thing: Item | Planet | Resource): HTMLAnchorElement => {
+    Draw: (thing: Thing): HTMLAnchorElement => {
         const link = document.createFullElement('a', {class: 'thing', href: Router.GetURL(thing)});
         link.appendChild(Utilities.DrawImage(thing));
         if(thing.type === ThingType.Item || thing.type === ThingType.Resource) {
@@ -25,7 +33,7 @@ export const Utilities = {
         }
         return link;
     },
-    DrawImage: (thing: Item | Planet | Resource): HTMLImageElement => {
+    DrawImage: (thing: Thing): HTMLImageElement => {
         return document.createFullElement('img', {src: Database.GetThingImage(thing)});
     },
 }
