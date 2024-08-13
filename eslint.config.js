cb47ff33d4c160951013ca6f5146d146a141eb71
Matthieu Corageoud <matthieu.corageoud@gmail.com>
2024-08-13 21:57:51 +0200

    Update ESLint to version 9

---
+++ eslint.config.js
@@ -0,0 +1,55 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import globals from 'globals';
+
+export default tseslint.config(
+	{
+		extends: [
+			js.configs.recommended,
+			...tseslint.configs.recommended,
+		],
+		ignores: ['dist/*', 'src/tools/*', 'webpack*'],
+		plugins: {
+			'@typescript-eslint': tseslint.plugin,
+		},
+		languageOptions: {
+			sourceType: 'module',
+			globals: {
+				...globals.browser,
+			},
+			parser: tseslint.parser,
+			parserOptions: {
+				requireConfigFile: false,
+				sourceType: 'module',
+				project: ['./tsconfig.json']
+			}
+		},
+		rules: {
+			'array-callback-return': 'error',
+			'consistent-return': 'error',
+			'curly': 'error',
+			'eol-last': 'error',
+			'eqeqeq': 'error',
+			'indent': ['error', 'tab', {'SwitchCase': 1}],
+			'key-spacing': 'error',
+			'linebreak-style': ['error', 'unix'],
+			'no-console': 'off',
+			'no-multi-spaces': 'error',
+			'no-multiple-empty-lines': 'error',
+			'no-new-wrappers': 'error',
+			'no-proto': 'error',
+			'no-prototype-builtins': 'off',
+			'no-undef': 'error',
+			'no-var': 'error',
+			'no-with': 'error',
+			'prefer-const': 'error',
+			'prefer-template': 'error',
+			'quotes': ['error', 'single'],
+			'semi': ['error', 'always'],
+			'spaced-comment': ['error', 'never'],
+			'strict': 'error',
+			'template-curly-spacing': ['error', 'never'],
+			'@typescript-eslint/unbound-method': 'off'
+		}
+	}
+);
