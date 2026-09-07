bad9328c9372c6fb672685201bb79f7d362ca160
Matthieu Corageoud <matthieu.corageoud@gmail.com>
2026-09-07 20:49:08 +0200

    Deep freeze the database to prevent mutations

--- src/repository.ts
+++ src/repository.ts
@@ -7,6 +7,17 @@ import {Thing} from './model/thing';
 
 let database: Database;
 
+//recursively freeze an object and all of its nested objects and arrays
+function deep_freeze<T>(value: T): T {
+	//only objects (including arrays) can be frozen; skip primitives and null
+	if(value !== null && typeof value === 'object') {
+		//freeze children before freezing the object itself
+		Object.values(value).forEach(deep_freeze);
+		Object.freeze(value);
+	}
+	return value;
+}
+
 function find_or_throw<T extends Thing>(things: T[], thing_id: string): T {
 	const thing = things.find(t => t.id === thing_id);
 	if(!thing) {
@@ -18,11 +29,13 @@ function find_or_throw<T extends Thing>(things: T[], thing_id: string): T {
 const Repository = {
 	Init: async() => {
 		const response = await fetch('/data.json');
-		database = Object.seal((await response.json()) as Database);
+		database = (await response.json()) as Database;
 		//add type to all things
 		database.resources.forEach(r => r.type = ThingType.Resource);
 		database.items.forEach(i => i.type = ThingType.Item);
 		database.planets.forEach(p => p.type = ThingType.Planet);
+		//deep freeze the database once it is fully initialized to prevent any further mutations
+		deep_freeze(database);
 	},
 	GetAll: (): Thing[] => [...database.resources, ...database.items, ...database.planets],
 	GetThings: (type: ThingType): Thing[] => {
